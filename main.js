b66acdce65dc3aff1f5412809e8ab198ba3ed4fc
Fares Harrazi <m.fares.harrazi@gmail.com>
2024-02-12 15:07:09 +0200

    function operate (step b)

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log('welcome to the calculator');
+
+// Function to perform operation based on operator and two numbers
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+    default:
+      return "Invalid operation";
+  }
+}
