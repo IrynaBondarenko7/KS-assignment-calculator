1888a095e8078f877065bfe4eb99671ab11bdb08
Thanzia <shaikhtanz@gmail.com>
2024-02-13 12:34:38 +0800

    updated the functions for basic  arithmetic operations

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 console.log("welcome to the calculator");
 
+// Functions to perform basic arithmetic operation
+function add(num1,num2){
+  return num1+num2;
+}
+function subtract(num1,num2){
+  return num1-num2;
+}
+function multiply(num1,num2){
+  return num1*num2;
+}
+function divide(num1,num2){
+  if(num2===0){
+      return "Error"
+  }
+  return num1/num2;
+}
+
 const ref = {
   displayBtn: document.querySelector("#display"),
   clearBtn: document.querySelector("#clear"),
